9bd5bc438b9e31c1260abf2a1f1af76468a125f1
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2025-07-09 10:38:48 -0400

    created collapsible navbar and updated some small styles

--- frontend/src/components/DocsNavbar.tsx
+++ frontend/src/components/DocsNavbar.tsx
@@ -1,10 +1,37 @@
 import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import { NAVBAR_ITEMS, type NavLink } from "../constants/NavLinks";
+import arrow from "../assets/images/right-arrow.png";
 
 import "../styles/Navbar.css";
-import { NAVBAR_ITEMS, type NavLink } from "../constants/NavLinks";
 
 export default function DocsNavbar() {
   const location = useLocation();
+  const [isHidden, setIsHidden] = useState(true);
+
+  const handleActivatorClick = () => {
+    setIsHidden(false);
+  };
+
+  const handleCloseMenu = () => {
+    setIsHidden(true);
+  };
+
+  useEffect(() => {
+    if (!isHidden) {
+      // Menu is open - prevent body scroll
+      document.body.style.overflow = "hidden";
+    } else {
+      // Menu is closed - restore body scroll
+      document.body.style.overflow = "unset";
+    }
+
+    // Cleanup function to restore scroll on unmount
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, [isHidden]);
 
   const renderNavLink = (subItem: NavLink) => {
     const commonClasses = `navbar-link-container ${
@@ -26,7 +53,12 @@ export default function DocsNavbar() {
     }
 
     return (
-      <Link to={subItem.url} className={commonClasses} key={subItem.id}>
+      <Link
+        to={subItem.url}
+        className={commonClasses}
+        key={subItem.id}
+        onClick={handleCloseMenu}
+      >
         <span className="navbar-link">{subItem.name}</span>
       </Link>
     );
@@ -34,7 +66,30 @@ export default function DocsNavbar() {
 
   return (
     <>
-      <div className="navbar-container">
+      <div
+        className={`navbar-container-activator${
+          isHidden ? "" : " navbar-container-activator--hidden"
+        }`}
+        onClick={handleActivatorClick}
+      >
+        <button className="mini-tab">
+          <span className="arrow-right"></span>
+        </button>
+      </div>
+      <div
+        className={`navbar-container${
+          isHidden ? "" : " navbar-container--active"
+        }`}
+      >
+        <div className="close-navbar-container" onClick={handleCloseMenu}>
+          <img
+            src={arrow}
+            alt="arrow to close menu"
+            style={{ height: "25px", width: "25px" }}
+            className="close-navbar-arrow"
+          ></img>
+        </div>
+
         {NAVBAR_ITEMS.map((item) => (
           <div key={item.id} className="navbar-section-container">
             <p className="navbar-section-title">{item.title}</p>
