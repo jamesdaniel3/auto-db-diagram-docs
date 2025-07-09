22ec752cd88a4c58439e8295618e3df55096f09e
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2025-07-08 20:35:16 -0400

    created the Linux Installation Page

--- frontend/src/components/pages/LinuxInstallation.tsx
+++ frontend/src/components/pages/LinuxInstallation.tsx
@@ -1,7 +1,37 @@
+import "../../styles/Pages.css";
+
 export default function LinuxInstallation() {
   return (
     <>
-      <p>This is the linux page</p>
+      <p className="page-title">Linux Installation</p>
+      <p className="page-text">
+        There is not currently an officail distribution for Auto DB Diagram
+        available on Linux, but it is tested on Ubunut 22.04 and 24.04.
+      </p>
+      <p className="page-text">
+        Because this is an open-source project, it can be{" "}
+        <a
+          className="page-intext-link"
+          target="_blank"
+          href="https://github.com/jamesdaniel3/auto-db-diagram"
+        >
+          cloned from Github
+        </a>{" "}
+        and run that way!
+      </p>
+      <p className="page-text">
+        After you have cloned the code, you can run the program with the
+        following commands
+      </p>
+      <div className="terminal-command-container">
+        <p className="terminal-command-text">
+          <span className="terminal-command-prompt">$ </span> cd [your
+          directory]
+        </p>
+        <p className="terminal-command-text">
+          <span className="terminal-command-prompt">$ </span> ./main.sh
+        </p>
+      </div>
     </>
   );
 }
